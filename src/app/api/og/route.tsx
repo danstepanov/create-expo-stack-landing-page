cbb9625f3bc64cc41234a8646bca4faa47d1a4d1
Dan Stepanov <dan@danstepanov.com>
2023-09-05 16:26:08 -0600

    [WIP] gradient background for og image

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -7,7 +7,6 @@ export const runtime = 'edge';
 export async function GET() {
   return new ImageResponse(
     (
-      // Modified based on https://tailwindui.com/components/marketing/sections/cta-sections
       <div
         style={{
           height: '100%',
@@ -16,34 +15,10 @@ export async function GET() {
           flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center',
-          backgroundColor: 'white',
         }}
       >
-        <div tw="bg-gray-50 flex">
-          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-            <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
-              <span>Ready to dive in?</span>
-              <span tw="text-indigo-600">Start your free trial today.</span>
-            </h2>
-            <div tw="mt-8 flex md:mt-0">
-              <div tw="flex rounded-md shadow">
-                <a
-                  href="#"
-                  tw="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white"
-                >
-                  Get started
-                </a>
-              </div>
-              <div tw="ml-3 flex rounded-md shadow">
-                <a
-                  href="#"
-                  tw="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600"
-                >
-                  Learn more
-                </a>
-              </div>
-            </div>
-          </div>
+        <div tw={`flex flex-col h-full w-full bg-gradient-to-b from-red-400 via-[#fb9300] to-yellow-500`}>
+          <p>Create Expo Stack</p>
         </div>
       </div>
     ),
