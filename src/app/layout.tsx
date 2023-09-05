b03a81aeac47e10a6228c32d2d3d1d6765947300
Dan Stepanov <dan@danstepanov.com>
2023-09-05 12:09:43 -0600

    initialize a generate og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,6 @@ import Nav from "./components/Nav";
 
 const geologica = Geologica({ subsets: ["latin"] });
 
-export const metadata = {
-	title: "Expo Stack",
-	description: "The easiest way to create a React Native app with Expo",
-};
-
 export default function RootLayout({
 	children,
 }: {
@@ -17,6 +12,13 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<head>
+				<meta property="og:image" content="<generated>" />
+				<meta property="og:image:alt" content="The easiest way to create a React Native app with Expo" />
+				<meta property="og:image:type" content="image/png" />
+				<meta property="og:image:width" content="1200" />
+				<meta property="og:image:height" content="630" />
+			</head>
 			<body
 				className={`flex flex-col bg-gradient-to-b from-red-400 via-[#fb9300] to-yellow-500 ${geologica.className}`}
 			>
