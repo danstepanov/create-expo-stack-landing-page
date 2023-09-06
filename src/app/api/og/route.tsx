bc23bb73cd2d01c5cd67aed06dbf8e48cc0c92d4
Dan Stepanov <dan@danstepanov.com>
2023-09-05 19:54:29 -0600

    working version of og image

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -5,6 +5,10 @@ import { ImageResponse } from 'next/server';
 export const runtime = 'edge';
  
 export async function GET() {
+  const fontData = await fetch(
+    new URL('../../Inter-ExtraBold.ttf', import.meta.url),
+  ).then((res) => res.arrayBuffer());
+
   return new ImageResponse(
     (
       <div
@@ -13,18 +17,35 @@ export async function GET() {
           width: '100%',
           display: 'flex',
           flexDirection: 'column',
+          fontFamily: 'Inter',
           alignItems: 'center',
           justifyContent: 'center',
+          background: "black"
         }}
       >
-        <div tw={`flex flex-col h-full w-full bg-gradient-to-b from-red-400 via-[#fb9300] to-yellow-500`}>
-          <p>Create Expo Stack</p>
+        <div tw="flex flex-col h-full w-full items-center justify-center">
+          <div tw="flex flex-row font-sans text-7xl text-center text-white w-9/12 flex-wrap justify-center">
+            <div>The easiest way to</div>
+            <div>create a</div>
+            <div style={{ width: '1rem' }}></div>
+            <div style={{ background: "linear-gradient(90deg, #5f14ff 0%, #2bbefd 100%)", backgroundClip: 'text', color: 'transparent' }}>React Native</div>
+            <div style={{ width: '1rem' }}></div>
+            <div>app using</div>
+            <div style={{ width: '1rem' }}></div>
+            <div style={{ background: "linear-gradient(90deg, #ffa914 0%, #fd402b 100%)", backgroundClip: 'text', color: 'transparent' }}>Expo</div>
+          </div>
         </div>
       </div>
     ),
     {
       width: 1200,
       height: 630,
+      fonts: [
+        {
+          name: 'Inter',
+          data: fontData
+        },
+      ],
     },
   );
 }
