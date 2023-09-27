ad794d92235b2f3135565f6edf9ef91f209efe8e
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-09-27 02:49:45 -0400

    components

---
+++ src/app/components/GithubStarButton.tsx
@@ -0,0 +1,42 @@
+const GithubStarButton = () => {
+  return (
+    <button className="text-sm md:text-base group border border-orange-400 bg-orange-900/50 rounded-full p-1 px-2 mx-auto flex gap-2 items-center text-orange-50 font-thin duration-300 hover:bg-orange-700/50 hover:border-orange-300 shadow-[0_0_0.5rem_0] shadow-orange-500 hover:shadow-[0_0_1.5rem_0] hover:shadow-orange-400">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="duration-300 group-hover:rotate-180 lucide lucide-sparkles"
+      >
+        <path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z" />
+        <path d="M5 3v4" />
+        <path d="M19 17v4" />
+        <path d="M3 5h4" />
+        <path d="M17 19h4" />
+      </svg>
+      <span className="hidden sm:block">Give us a star on Github</span>
+      <span className="sm:hidden">Star us on Github</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="duration-300 group-hover:translate-x-0.5 lucide lucide-chevron-right"
+      >
+        <path d="m9 18 6-6-6-6" />
+      </svg>
+    </button>
+  );
+};
+
+export default GithubStarButton;
