ad3cfff89937e76a66a6250c6ff3eaa56e03daa4
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-10-03 15:11:23 -0400

    add comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,9 @@ export default function Home() {
     <main className="flex flex-col items-center w-full relative overflow-hidden">
       <BackgroundSVG />
       <Nav />
+      {/* everything is z-1 to sit on top of <BackgroundSVG /> */}
       {/* aspect-square container to match dimensions of aspect-square BackgroundSVG */}
-      <section className="flex flex-col items-center w-full relative z-[1] aspect-square gap-16 pt-14">
+      <section className="z-[1] flex flex-col items-center w-full relative aspect-square gap-16 pt-14">
         <h1 className="w-10/12 max-w-screen-lg text-center text-4xl font-bold tracking-tight text-transparent sm:text-6xl lg:text-[4rem] xl:text-[4rem] bg-clip-text bg-gradient-to-t from-zinc-400 to-33% to-white">
           The most configurable way to create an{" "}
           <span className="bg-gradient-to-br from-yellow-500 to-orange-500 bg-clip-text text-transparent drop-shadow-xl">
