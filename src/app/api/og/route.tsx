9d13f21ed31d90f23adea6c3fd4dfdcfbcdaf008
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-10-14 21:52:20 -0400

    tweak og image

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -35,6 +35,7 @@ export async function GET() {
             aspectRatio: "1/1",
             position: "absolute",
             transform: "translate(-50%, -50%)",
+            opacity: "0.5",
           }}
           xmlns="http://www.w3.org/2000/svg"
         >
@@ -42,13 +43,13 @@ export async function GET() {
             d="M96 0.00402832H24C10.7452 0.00402832 0 10.7492 0 24.004V96.004C0 109.259 10.7452 120.004 24 120.004H96C109.255 120.004 120 109.259 120 96.004V24.004C120 10.7492 109.255 0.00402832 96 0.00402832Z"
             stroke="white"
             stroke-opacity="0.1386"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M38.0425 82.6074C25.5685 70.48 25.288 50.5376 37.4151 38.0647C49.5425 25.5907 69.4849 25.3102 81.9578 37.4373C94.4318 49.5647 94.7123 69.5071 82.5852 81.9801C70.6202 94.2876 51.0037 94.7493 38.4726 83.0184"
             stroke="white"
             stroke-opacity="0.1386"
-            stroke-width="0.5"
+            stroke-width="0.25"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
@@ -56,7 +57,7 @@ export async function GET() {
             d="M23.4005 97.6867C2.61083 77.4745 2.14321 44.2376 22.3549 23.4496C42.567 2.65987 75.804 2.1923 96.592 22.404C117.382 42.6161 117.849 75.8531 97.6376 96.6411C77.6961 117.154 45.0023 117.923 24.1174 98.3716"
             stroke="white"
             stroke-opacity="0.1386"
-            stroke-width="0.5"
+            stroke-width="0.25"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
@@ -64,7 +65,7 @@ export async function GET() {
             d="M44.6614 75.8002C35.9494 67.3302 35.7534 53.4022 44.2232 44.6909C52.6932 35.9789 66.6213 35.7829 75.3326 44.2527C84.0446 52.7227 84.2405 66.6508 75.7708 75.3621C67.4142 83.9579 53.7137 84.2803 44.9618 76.0873"
             stroke="white"
             stroke-opacity="0.1386"
-            stroke-width="0.5"
+            stroke-width="0.25"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
@@ -72,73 +73,73 @@ export async function GET() {
             d="M0.494019 38.004H119.504"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M0.494019 82.004H119.504"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M59.9969 0.494019V119.504"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M81.9969 0.494019V119.504"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M37.9969 0.494019V119.504"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M0.494019 60.004H119.504"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M0.494019 112.454H119.504"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M0.494019 7.50403H119.504"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M112.497 0.494019V119.504"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M7.5 0.494019V119.504"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M0.5 119.454L119.5 0.504028"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
           <path
             d="M119.497 119.454L0.496948 0.504028"
             stroke="white"
             stroke-opacity="0.1292"
-            stroke-width="0.5"
+            stroke-width="0.25"
           />
         </svg>
         <div tw="flex flex-col h-full w-full items-center justify-center">
@@ -241,16 +242,24 @@ export async function GET() {
               </filter>
             </defs>
           </svg>
-          <div tw="flex flex-row font-sans text-7xl text-center text-white/50 w-9/12 flex-wrap justify-center">
+          <div tw="flex flex-row font-sans text-7xl text-center text-white w-9/12 flex-wrap justify-center">
             <div>The</div>
             <div style={{ width: "1rem" }}></div>
-            <div tw="text-white" style={{ textShadow: "0 0 2rem #fff8" }}>
+            <div
+              tw="text-white"
+              style={{
+                textShadow: "0 0 2rem #fff8",
+                background: "linear-gradient(90deg, #888fff 0%, #ff88ff 100%)",
+                backgroundClip: "text",
+                color: "transparent",
+              }}
+            >
               most configurable
             </div>
             <div style={{ width: "1rem" }}></div>
             <div>way to create an</div>
           </div>
-          <div tw="flex flex-row font-sans text-7xl text-center text-white/50 w-9/12 flex-wrap justify-center">
+          <div tw="flex flex-row font-sans text-7xl text-center text-white w-9/12 flex-wrap justify-center">
             <div
               style={{
                 background: "linear-gradient(90deg, #f97316 0%, #eab308 100%)",
